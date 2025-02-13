1035fd832c44ec83c6d2a5ae44644a8571190ac3
Alex Ruiz Chazarra <alexruicha@gmail.com>
2025-02-13 18:34:39 +0100

    a

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}", // O ajusta la ruta según tu estructura de carpetas
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
